fa80f2b167eb3f87717b48169cb6a7a0e53be5d0
brilee8820 <brilee8820@gmail.com>
2018-07-11 15:34:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
-  if int % 3 && 5 == 0
+  if int % 3 && 5 int % 5 == 0
     puts "Fizzbuzz"
-  elsif int % 3 == 0 
-    puts "Fizz"
-  elsif int % 5 == 0
+  elsif int % 5 == 0 
     puts "Buzz"
+  elsif int % 3 == 0
+    puts "Fizz"
   else
 end
 end
